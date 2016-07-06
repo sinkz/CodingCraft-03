8e3e33c197180a10d522681c8d4eac0f16eced81
Adiego <diih_augusto@live.com>
2016-07-06 15:54:33 -0300

    Add project files.

---
+++ CodingCraft-03/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CodingCraft_03.MvcApplication" Language="C#" %>
---
+++ CodingCraft-03/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CodingCraft_03.Startup))]
+namespace CodingCraft_03
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
